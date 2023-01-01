3a18a44684c730e77ba0f7bbbacdeed684626db3
Reetta Ylitalo <reetta.k.ylitalo@helsinki.fi>
2023-01-01 08:32:25 +0200

    Use bootstrap collapse for toggling elements.

--- static/javascript.js
+++ static/javascript.js
@@ -4,16 +4,6 @@ setTimeout(() => {
         resultbox.remove();
 }, 5000);
 
-
-function toggleVisibility(elementId) {
-    var element = document.getElementById(elementId);
-
-    // Display is <empty string> on the first click. Make display block in that case, also.
-    element.style.display === "none" || element.style.display === "" 
-        ? element.style.display = "block" 
-        : element.style.display = "none"
-}
-
 // Multiselect allowed for dropdown lists.
 $(document).ready(function() {
     $(".selectpicker").selectpicker();
